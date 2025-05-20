e93321e1dddccf7ecb40a2d8f26dccb7df6a69e8
prasanth-baskar-39 <prasanthbaskar39@gmail.com>
2025-05-20 07:22:36 -0400

    adding request files

---
+++ requests_url.py
@@ -0,0 +1,12 @@
+import requests
+
+# Replace with the desired URL
+for i in range(10001):
+    url = "https://www.cloudoplus.com"
+
+    # Make a GET request
+    response = requests.get(url)
+
+    # Print status code and content
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Body:\n{response.text}")
